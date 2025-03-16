a48b8f32e810822481392397d0d7d6a19421b29c
Caleb Melnychenko <calebinski98@gmail.com>
2025-03-16 15:44:22 -0400

    Caleb - Javascript tests

---
+++ src/problems.js
@@ -0,0 +1,28 @@
+class Problems {
+    constructor() { }
+
+    // Problem 1: Sum the entire array and return the answer
+    sumArray(array) {
+        let ans = 0;
+
+        return ans;
+    }
+
+    // Problem 2: Capitalizing all words throughout the string
+    // Example - Input: "hello world" --> Output: "Hello World"
+    // Example 2 - Input: "hEllO WORLD" --> Output: "Hello World"
+    capitalizeWords(str) {
+        return str;
+    }
+
+    // Problem 3: Match the list of foods with the list of prices
+    // Output should be in a Map
+    // If there are more foods than prices then do not include the foods without a price
+    // If there are more prices than foods then ignore the extra prices in the final map
+    createGroceryList(foods, prices) {
+        return new Map();
+    }
+
+}
+
+module.exports = Problems;
---
+++ test/problems.test.js
@@ -0,0 +1,63 @@
+const problem = require('../src/problems')
+
+let prob;
+
+beforeAll(() => {
+    prob = new problem();
+});
+
+test('Problem 1: Sum Array', () => {
+    expect(prob.sumArray([1,2,3,4,5])).toBe(15);
+    expect(prob.sumArray([10,20,30,40,50])).toBe(150);
+    expect(prob.sumArray([10,1,4,3,2])).toBe(20);
+    expect(prob.sumArray([-10,1,4,3,2])).toBe(0);
+    expect(prob.sumArray([-10,1,4,3,2])).toBe(0);
+    expect(prob.sumArray([25, 43, 9, -32, 18, 103, -65])).toBe(101);
+});
+
+test('Problem 2: Capitalize words', () => {
+
+    const test1 = prob.capitalizeWords('dog');
+    const test2 = prob.capitalizeWords('DOG');
+    const test3 = prob.capitalizeWords('the dog chased its tail.');
+    const test4 = prob.capitalizeWords('THE Dog chAseD its tAIl.');
+
+    expect(test1).toBe('Dog');
+    expect(test2).toBe('Dog');
+    expect(test3).toBe('The Dog Chased Its Tail.');
+    expect(test4).toBe('The Dog Chased Its Tail.');
+
+});
+
+test("Problem 3: Grocery List", () => {
+    const testFoods1 = ['Apple', 'Chicken', 'Milk', 'Eggs'];
+    const testPrices1 = [3, 10, 6, 8];
+
+    const testFoods2 = ['Apple', 'Chicken', 'Milk', 'Eggs', 'Cake'];
+    const testPrices2 = [3, 10, 6, 8, 15, 17, 20];
+
+    const testFoods3 = ['Apple', 'Chicken'];
+    const testPrices3 = [3, 10, 6, 8, 15, 17, 20];
+
+    const test1 = prob.createGroceryList(testFoods1, testPrices1);
+    const test2 = prob.createGroceryList(testFoods2, testPrices2);
+    const test3 = prob.createGroceryList(testFoods3, testPrices3);
+
+    expect(test1.get('Apple')).toBe(testPrices1[0]);
+    expect(test1.get('Chicken')).toBe(testPrices1[1]);
+    expect(test1.get('Milk')).toBe(testPrices1[2]);
+    expect(test1.get('Eggs')).toBe(testPrices1[3]);
+    expect(test1.get('Cake')).toBeUndefined();
+
+    expect(test2.get('Apple')).toBe(testPrices2[0]);
+    expect(test2.get('Chicken')).toBe(testPrices2[1]);
+    expect(test2.get('Milk')).toBe(testPrices2[2]);
+    expect(test2.get('Eggs')).toBe(testPrices2[3]);
+    expect(test2.get('Cake')).toBe(testPrices2[4]);
+
+    expect(test3.get('Apple')).toBe(testPrices3[0]);
+    expect(test3.get('Chicken')).toBe(testPrices3[1]);
+    expect(test1.get('Milk')).toBeUndefined();
+    expect(test1.get('Eggs')).toBeUndefined();
+    expect(test1.get('Cake')).toBeUndefined();
+});
