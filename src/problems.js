f64ad17d2049049ebba41ebef6cd95f08e345bde
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-04-23 13:28:01 -0400

    Mariah - attempting to create a json object for problem 6

--- src/problems.js
+++ src/problems.js
@@ -85,10 +85,9 @@ class Problems {
     * Assume that the eventPoints are in the correct order as the order of the athletes list.
     */
     assignAwards(athletes, eventPoints) {
-       
-
-       
-
+        let json {"participants": athletes,
+            "points": [eventPoints]
+        }
         return {};
     }
 
