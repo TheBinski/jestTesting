9e72a130ec83ea571056a5429c9f853991f8a0fe
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-05-03 13:29:24 -0400

    Mariah - I thought I understood the lesson. Apparently not; changed the second for loop how i thought it should be as well as added to totals the way i thought was correct.

--- src/problems.js
+++ src/problems.js
@@ -110,12 +110,12 @@ class Problems {
     sumAllArrays(arrays) {
         let totals = [];
         let sum = 0;
-        for(let i =0; i<arrays.length;i++){
-            for(let j=0; j<arrays[i.length];j++){
+        for(let i =0; i<=arrays.length;i++){
+            for(let j=0; j<arrays[i].length;j++){
                 sum += arrays[i][j];
+                totals[j] = sum;
             }
-
-            totals = [sum];
+           
         }
         return totals;
     }
