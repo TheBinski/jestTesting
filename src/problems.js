30653ac99394dcbe6fd1f3d089e8eb0d3676ee11
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-07-02 15:01:36 -0400

    Mariah - trying to figure out why deckOfCards is not splitting and rearranging

--- src/problems.js
+++ src/problems.js
@@ -262,10 +262,12 @@ class Problems {
     // If the deck is not an even number then leave the extra card on the bottom
     // Do NOT return the answer. This should affect the original deckOfCards just as it would with a real deck of cards.
     cutDeck(deckOfCards) {
+        console.log(deckOfCards);
         let midPoint = Math.ceil(deckOfCards.length);
         let secondHalf = deckOfCards.slice(midPoint);
         let firstHalf = deckOfCards.slice(0, midPoint);
         deckOfCards = secondHalf.concat(firstHalf);
+        console.log(deckOfCards);
 
     }
 
